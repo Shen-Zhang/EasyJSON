38ae6e3d7081669cd1bad709ce70e590ad87a49d
Shen-Zhang <zhangshe@grinnell.edu>
2014-04-16 19:08:09 -0500

    expo

--- src/zhangshe/json/JSONParserEXPT.java
+++ src/zhangshe/json/JSONParserEXPT.java
@@ -22,11 +22,7 @@ public class JSONParserEXPT
     String c =
         "{\"abc\":null,\"dsa\":3,\"ds\":[\"Hello\",\"hello\",\"abc\",\"231232\"]}";
     String d = "{\"a\":\"rebelsky\",\"b\":354,\"c\":3214,\"d\":32154}";
-    String e = "{\"name\":\"sam\",\"title\":\"prof\",\"alive\":true}";
-    String f =
-        "{\"name\":\"sam\",\"job\":{\"employer\":\"Grinnell\",\"title\":\"Professor\"},\"alive\":true}";
-    String h =
-        "{\"name\":\"sam\",\"grades\":{\"exam1\":80,\"exam2\":[85],\"final\":{\"A\":100}},\"letter\":\"B\"}";
+
     JSONValue val = JSON.parse(g);
 
     pen.println(val.toJSONString());
